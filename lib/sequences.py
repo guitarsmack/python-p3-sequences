e2ef9cdfbe11f65e4366100ff3694cf7ca47ddb3
Michael Alan Stowe <mastowe94@gmail.com>
2023-10-29 18:35:42 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+print("This works")
 
 def print_fibonacci(length):
+    if length < 2:
+        print([]) if length == 0 else print([0])
+        return
+    sequence =[0,1]
+    while len(sequence) < length:
+        num1 = sequence[-1]
+        num2 = sequence[-2]
+        sequence.append(num1+num2)
+    print(sequence)
     pass
+
+
+print_fibonacci(2)
